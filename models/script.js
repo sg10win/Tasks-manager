a1714450d8617d5709f4a5b93c684a93f5f29121
Segev Shalom <56483019+sg10win@users.noreply.github.com>
2025-01-17 22:10:57 +0200

    3 columns of tasks with a mode btn for each

--- models/script.js
+++ models/script.js
@@ -1,37 +1,47 @@
-const addBtn = document.querySelector('#push')
-addBtn.addEventListener('click',function(event){add(event)})
-addBtn.onclick(add())
-
-// At the beginning I have to do post request of all the data
-// how to save the data
-// how to get it from server in a propper way 
- 
-
-
-function add (event){
-    event.preventDefault()
-    const newTaskInput = document.querySelector('#newtask input')
-    const newTaskValue = newTaskInput.value
-    if (newTaskValue.length == 0){
-        alert('Please Enter a Task')
-    }
-    else{
-        tasks = document.querySelector('#tasks');
-        tasks.innerHTML
-        += `<div class="task">
-                <span id="taskname">
-                    ${newTaskValue}
-                </span>
-                <button class="delete">Delete</button>
-            </div>`;
-        var currentTasks = document.querySelectorAll(".delete");
-        console.log(currentTasks.length)
-        for(var i=0;i<currentTasks.length;i++){
-            var current = currentTasks[i]
-            current.onclick = function(){
-                this.parentNode.remove()
-            }
-        }
-        newTaskInput.value = ''
-    }
+const addBtns = document.querySelectorAll('.push')
+addBtns.forEach(element => {
+    element.onclick = function(){addTask(element)}
+    
+});
+function deleteTask(deleteBtn){
+    deleteBtn.parentNode.remove()
 }
+function changeMode(modeBtn){
+    let colors = ['', 'red', 'yellow', 'green']
+    let taskColors = ['', 'rgb(252, 184, 175)', 'rgb(250, 240, 153)', '#a6fca6']
+    modeBtn.value = (parseInt(modeBtn.value)+1)%4
+    modeBtn.textContent = `Mode_${modeBtn.value}`
+    modeBtn.style.backgroundColor = colors[modeBtn.value]
+    modeBtn.parentNode.style.backgroundColor = taskColors[modeBtn.value]
+}
+
+
+// element => Add button (.push)
+function addTask(element){
+    var className = element.classList[1]
+    var inputField = document.getElementsByClassName(`input ${className}`)[0];
+    var inputVal = inputField.value
+    if (inputVal.length == 0){
+        alert('Enter a task')
+        return
+    }
+    inputField.value = ''
+    var tasks = document.getElementsByClassName(`tasks ${className}`)[0]
+    tasks.innerHTML += (`
+                    <div class="task ${className}">
+                        <span id="taskname">
+                        ${inputVal}
+                        </span>
+                        <button class="delete">Delete</button>
+                        <button class="mode" value=0>Mode_0</button>
+                    </div>
+                    `)
+    const deleteBtns = document.getElementsByClassName('delete')
+    const modeBtns = document.getElementsByClassName('mode')
+    for(var i=0;i<deleteBtns.length;i++){
+        deleteBtns[i].onclick = function(){deleteTask(this)}
+        modeBtns[i].onclick = function(){changeMode(this)}
+    }
+    
+}
+
